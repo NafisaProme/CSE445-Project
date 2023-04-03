0b889d117c8dd939a9de4b723f48143dde6c3466
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-04-04 01:48:06 +0600

    laptop dataset collected from ryans

--- dataset/ryans.py
+++ dataset/ryans.py
@@ -1,27 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
-from csv import writer
+import csv
 
 # Send a GET request to the website
-url = 'https://www.ryanscomputers.com/category/laptop-all-laptop?limit=5&osp=0'
+url = 'https://www.ryanscomputers.com/category/laptop-all-laptop?limit=20&osp=0'
 response = requests.get(url)
-
-output_file = open('dataset\\ryans.csv', 'w')
-
 
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Find all the href links in the product descriptions
-laptop_links = []
-for laptop in soup.find_all('p', class_='card-text p-0 m-0 list-view-text'):
-    laptop_link = laptop.find('a')['href']
+# opening the file and writing to it
+with open('dataset/ryans.csv', 'w', newline='') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
-    laptop_response = requests.get(laptop_link)
-    laptop_html = laptop_response.text
-    laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
+    # writing the heading of all the laptops
+    for laptop in soup.find_all('p', class_='card-text p-0 m-0 list-view-text'):
+        laptop_link = laptop.find('a')['href']
 
-    # Extract the laptop name, price, and specifications
-    name = laptop_soup.find('span', class_='att-value context').text + '\n'
-    output_file.write(name)
-    print(name)
+        laptop_response = requests.get(laptop_link)
+        laptop_html = laptop_response.text
+        laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
+
+        # Extract the laptop name, price, and specifications
+        heading = laptop_soup.find_all('span', class_='att-title context')
+        info = []
+
+        for head in heading:
+            info.append(head.text)
+
+        wr.writerow(info)
+        break
+
+    # writing the properties of each laptop
+    for laptop in soup.find_all('p', class_='card-text p-0 m-0 list-view-text'):
+        laptop_link = laptop.find('a')['href']
+
+        laptop_response = requests.get(laptop_link)
+        laptop_html = laptop_response.text
+        laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
+
+        # Extract the laptop name, price, and specifications
+        detail = laptop_soup.find_all('span', class_='att-value context')
+        info = []
+
+        for data in detail:
+            info.append(data.text)
+
+        wr.writerow(info)
