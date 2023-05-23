0798801643fa7c1f1eeda7b2d4b615c5918bc434
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-05-23 20:10:24 +0600

    dataset merge

--- dataset/Startech.py
+++ dataset/Startech.py
@@ -4,7 +4,7 @@ from csv import writer
 
 # to get access to the website
 
-url = "https://www.startech.com.bd/laptop-notebook/laptop?limit=1&page=1"
+url = "https://www.startech.com.bd/laptop-notebook/laptop?limit=90&page=1"
 # request to the website
 page = requests.get(url)
 
@@ -14,6 +14,7 @@ soup = BeautifulSoup(page.content, 'html.parser')
 laptop_links = []
 headings = ['Price', 'Brand']
 
+cnt = 1
 with open('dataset/Startech.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     for laptop in soup.find_all('h4', class_='p-item-name'):
@@ -25,17 +26,19 @@ with open('dataset/Startech.csv', 'w', encoding='utf8', newline='') as f:
         laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
 
         heading = laptop_soup.find_all('td', class_='name')
+
+        if cnt == 3:
+            for head in heading:
+                headings.append(head.text)
+
+            thewriter.writerow(headings)
+            break
         
+        cnt += 1
 
-        for head in heading:
-            headings.append(head.text)
-
-        thewriter.writerow(headings)
-        break
-
-with open('dataset/startech.csv', 'a', encoding='utf8', newline='') as f:
+with open('dataset/Startech.csv', 'a', encoding='utf8', newline='') as f:
     thewriter = writer(f)
-    for page_num in range(1,2):
+    for page_num in range(1,9):
         url = url[:-1]
         url = url + str(page_num)
         page = requests.get(url)
@@ -72,39 +75,33 @@ with open('dataset/startech.csv', 'a', encoding='utf8', newline='') as f:
                 body = heading.find('ul')
                 ans = body.find_all('li')
 
-                for item in ans:
-                    name = item.text
-                    if "Model: " in name:
-                        info.append(name[7:])
+                # for item in ans:
+                #     name = item.text
+                #     if "Model: " in name:
+                #         info.append(name[7:])
+
                 heading = laptop_soup.find_all('td', class_='name')
                 lists = laptop_soup.find_all('div', class_='product-details content')
                         
-                # for list in lists:
-                #     num = list.find_all('td', class_='value')
-                #     for i in num:
-                #         info.append(i.text)
+                for list in lists:
+                    num = list.find_all('td', class_='value')
+                    ind = 2
 
+                    for data in range(len(num)):
+                        x = headings[ind]
+                        y = heading[data].text.strip()
                 
-                ind = 1
-                for data in range(len(lists)):
-                    # print(data)
-                    x = headings[ind]
-                    y = heading[data].text.strip()
-
-                    # checking whether the headings match, to decide whether to add the value or the NULL value 
-                    if x == y:
-                        info.append(lists[data].text.strip())
-                    else:
+                        # checking whether the headings match, to decide whether to add the value or the NULL value 
+                        if x == y:
+                            info.append(num[data].text.strip())
+                        else:
+                            ind += 1
+                            info.append("NULL")
+                            info.append(num[data].text.strip())
+                        
                         ind += 1
-                        info.append("NULL")
-                        info.append(lists[data].text.strip())
-                    
-                    ind += 1
-                    if ind >= len(headings):
-                        break
+                        if ind >= len(headings):
+                            break
 
- 
-    
-                    
-                
-        thewriter.writerow(info)
+                    thewriter.writerow(info)
+
--- dataset/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('copy.csv', on_bad_lines='skip')
-df.head()
-df_filled = df.fillna('null')
-print(df_filled)
