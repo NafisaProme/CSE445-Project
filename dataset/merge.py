b40eb5650de68fa275fee9c0cd610c759fe8c472
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-06-06 19:02:30 +0600

    testing

--- dataset/merge.py
+++ dataset/merge.py
@@ -3,8 +3,8 @@ import pandas as pd
 # Assuming you have two datasets: df1 and df2
 
 # Specify the actual datasets to merge
-df1 = pd.read_csv('dataset\ryans.csv')
-df2 = pd.read_csv('dataset\Startech.csv')
+df1 = pd.read_csv('dataset\\ryans.csv',on_bad_lines='skip')
+df2 = pd.read_csv('dataset\Startech.csv',on_bad_lines='skip')
 
 # Merge the datasets based on a common column
 merged_df = pd.merge(df1, df2, on='RAM')
