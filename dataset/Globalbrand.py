4be872331002fb408fe9dfa8a34feff5fb590d2a
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-06-06 11:28:44 +0600

    techland dataset updated

--- dataset/Globalbrand.py
+++ dataset/Globalbrand.py
@@ -4,14 +4,15 @@ from csv import writer
 import re
 
 # Send a GET request to the website
-url = "https://www.globalbrand.com.bd/all-laptop?limit=1000"
+url = "https://www.globalbrand.com.bd/all-laptop?limit=5"
 response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 laptop_links=[]
+headings = ['Price']
 
-with open('Globalbrand.csv', 'w', encoding='utf8', newline='') as f:
+with open('dataset/copy.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     for laptop in soup.find_all('div', class_='name'):
         laptop_link = laptop.find('a')['href']
@@ -22,53 +23,53 @@ with open('Globalbrand.csv', 'w', encoding='utf8', newline='') as f:
         laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
 
         heading1 = laptop_soup.find_all('td')
-        info = []
-        info.append('Price')
         
         for head in heading1:
-            info.append(head.text)
-
-        thewriter.writerow(info)
-        break
-
-with open('Globalbrand.csv', 'a', encoding='utf8', newline='') as f:
-    thewriter = writer(f)
-
-
-
-    for laptop in soup.find_all('div', class_='name'):
-        laptop_link = laptop.find('a')['href']
-        laptop_links.append(laptop_link)
-
-        laptop_response = requests.get(laptop_link)
-        laptop_html = laptop_response.text
-        laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
-
-        info = []
-        
-        price = laptop_soup.find('div', class_='product-price')
-        if price != None:
-
-            for cost in price:
-                work = cost.text
-                name = ''
-                for i in work:
-                    if i != ',' and i != '৳':
-                         name = name + i
-                         
-                info.append(name)
-       
-        lists = laptop_soup.find_all('table', class_="table table-bordered")
+            take = head.text 
+            if ':' in take:
+                take = take.split(':')[0]
+                headings.append(take)
                 
-        for list in lists:
-            num = list.find_all('td')
-            for n in num:
-                text = n.text
-                if ":" in text:
-                    value = text.split(":")[1].strip()
-                    print(value)
-                    info.append(value)
+    headings = list(dict.fromkeys(headings))
+    thewriter.writerow(headings)
+    print(headings)
+    print(len(headings))
 
+# with open('dataset/copy.csv', 'a', encoding='utf8', newline='') as f:
+#     thewriter = writer(f)
+
+
+
+#     for laptop in soup.find_all('div', class_='name'):
+#         laptop_link = laptop.find('a')['href']
+#         laptop_links.append(laptop_link)
+
+#         laptop_response = requests.get(laptop_link)
+#         laptop_html = laptop_response.text
+#         laptop_soup = BeautifulSoup(laptop_html, 'html.parser')
+
+#         info = []
         
-           
-        thewriter.writerow(info)
+#         price = laptop_soup.find('div', class_='product-price')
+#         if price != None:
+
+#             for cost in price:
+#                 work = cost.text
+#                 name = ''
+#                 for i in work:
+#                     if i != ',' and i != '৳':
+#                         name = name + i
+                         
+#                 info.append(name)
+       
+#         lists = laptop_soup.find_all('table', class_="table table-bordered")
+                
+#         for list in lists:
+#             num = list.find_all('td')
+#             for n in num:
+#                 text = n.text
+#                 if ":" in text:
+#                     value = text.split(":")[1].strip()
+#                     info.append(value)
+       
+#         thewriter.writerow(info)
--- dataset/techland_fix.py
+++ dataset/techland_fix.py
@@ -4,7 +4,7 @@ from csv import writer
 
 # to get access to the website
 
-url = "https://www.techlandbd.com/shop-laptop-computer/brand-laptops?limit=10&page=1"
+url = "https://www.techlandbd.com/shop-laptop-computer/brand-laptops?limit=3&page=1"
 # request to the website
 page = requests.get(url)
 
@@ -12,7 +12,7 @@ page = requests.get(url)
 # create an object
 soup = BeautifulSoup(page.content, 'html.parser')
 laptop_links = []
-headings = ['Price']
+headings = ['Price', 'Processor Brand', 'Processor Model', 'Generation', 'RAM']
 
 with open('dataset/copy.csv', 'w', encoding='utf8', newline='') as f:
     thewriter = writer(f)
@@ -26,8 +26,10 @@ with open('dataset/copy.csv', 'w', encoding='utf8', newline='') as f:
 
         heading = laptop_soup.find_all('td', class_='attribute-name')
 
+        leave = ['Processor', 'Memory']
         for head in heading:
-            headings.append(head.text)
+            if head.text not in leave:
+                headings.append(head.text)
 
     headings = list(dict.fromkeys(headings))
     thewriter.writerow(headings)
@@ -38,7 +40,7 @@ with open('dataset/copy.csv', 'a', encoding='utf8', newline='') as f:
     thewriter = writer(f)
     for page_num in range(1, 2):
         # changing the urls of each page accordingly
-        url = "https://www.techlandbd.com/shop-laptop-computer/brand-laptops?limit=10&page=" + str(page_num)
+        url = "https://www.techlandbd.com/shop-laptop-computer/brand-laptops?limit=5&page=" + str(page_num)
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -71,38 +73,38 @@ with open('dataset/copy.csv', 'a', encoding='utf8', newline='') as f:
                         x = take_head.text
                         y = take_name.text
 
-                        # if x == 'Processor':
-                        #     if 'Intel' in y:
-                        #         pro_brand = y.split(' ')[0]
-                        #         pro_mix = y.split(' ')[1] + y.split(' ')[2]
-                        #         pro_model = 'Core i' + pro_mix.split('-')[0][5]
-                        #         pro_gen = pro_mix.split('-')[1][0] + pro_mix.split('-')[1][1] + 'th'
+                        if x == 'Processor':
+                            if 'Intel' in y:
+                                pro_brand = y.split(' ')[0]
+                                pro_mix = y.split(' ')[1] + y.split(' ')[2]
+                                pro_model = 'Core i' + pro_mix.split('-')[0][5]
+                                pro_gen = pro_mix.split('-')[1][0] + pro_mix.split('-')[1][1] + 'th'
 
-                        #         heading_map[headings[1]] = pro_brand
-                        #         heading_map[headings[2]] = pro_model
-                        #         heading_map[headings[3]] = pro_gen
-                        #     else:
-                        #         pro_brand = y.split(' ')[0]
-                        #         pro_model = y.split(' ')[1] + ' ' + y.split(' ')[2]
-                        #         heading_map[headings[1]] = pro_brand
-                        #         heading_map[headings[2]] = pro_model
+                                heading_map[headings[1]] = pro_brand
+                                heading_map[headings[2]] = pro_model
+                                heading_map[headings[3]] = pro_gen
+                            else:
+                                pro_brand = y.split(' ')[0]
+                                pro_model = y.split(' ')[1] + ' ' + y.split(' ')[2]
+                                heading_map[headings[1]] = pro_brand
+                                heading_map[headings[2]] = pro_model
                         
-                        # elif x == 'Memory':
-                        #     y = y.split(' ')[0]
-                        #     heading_map[headings[4]] = y
+                        elif x == 'Memory':
+                            y = y.split(' ')[0]
+                            heading_map[headings[4]] = y
                         
-                        # elif x == 'Storage':
-                        #     y = y.split(' ')[0] + " SSD"
-                        #     heading_map[x] = y
+                        elif x == 'Storage':
+                            y = y.split(' ')[0] + " SSD"
+                            heading_map[x] = y
 
-                        # elif x == 'Display':
-                        #     y = y.split(' ')[0]
-                        #     if len(y) == 2:
-                        #         y += '"'
-                        #     heading_map[x] = y
+                        elif x == 'Display':
+                            y = y.split(' ')[0]
+                            if len(y) == 2:
+                                y += '"'
+                            heading_map[x] = y
 
-                        # else:
-                        heading_map[x] = y
+                        else:
+                            heading_map[x] = y
 
                 heading_map['Price'] = final_cost
 
