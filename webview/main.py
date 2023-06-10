1492099506e76b31551350d3a945edbe5ade22cd
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-06-10 13:15:58 +0600

    intializing the batch file and reading from pickle

--- webview/main.py
+++ webview/main.py
@@ -1,29 +1,48 @@
 import streamlit as st
-st.sidebar.title("Welcome to LapTopPricePredix")
-@st.cache(suppress_st_warning=True)
-def get_fvalue(val):
-    feature_dict = {"No": 1, "Yes": 2}
-    for key, value in feature_dict.items():
-        if val == key:
-            return value
+import pickle
+import numpy as np
 
-def get_value(val, my_dict):
-    for key, value in my_dict.items():
-        if val == key:
-            return value
+# importing the model and the pickle dataset 
+df = pickle.load(open("dataset.pkl", "rb"))
 
-app_mode = st.sidebar.selectbox('Select Page', ['Home', 'Prediction'])  # two pages
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    # brand input
+    laptop_brand = st.selectbox("Brand", df["Brand"].unique())
 
-if app_mode=='Home':    
-    st.title('Laptop Price PREDICTION :') 
-    st.image('Images\laptop.jpg',width=600)
+# with middle_column:
+#     # laptop type
+#     type = st.selectbox("Type", df["TypeName"].unique())
 
-elif app_mode == 'Prediction':
-    st.image('Images\laptop.jpg',width=600)
-    st.text("Sir/Maam,YOU need to fill all the neccesary informations in order to know the price!")
+# with right_column:
+#     # Ram size
+#     ram = st.selectbox("Ram (in GB)", df["Ram"].unique())
 
-    st.sidebar.multiselect('choose a Brand', ['ASUS', 'Dell', 'HP', 'Apple'])
-    st.sidebar.multiselect('choose RAM',['8GB', '16GB', '32GB','64GB'])
-    st.sidebar.multiselect('choose Processor',['Intel', 'AMD'])
-    st.sidebar.multiselect('choose Processor Core ',['Core i3', 'Core i5', 'Core i7'])
-    st.button("Predict")
+# st.sidebar.title("Welcome to LapTopPricePredix")
+# @st.cache(suppress_st_warning=True)
+# def get_fvalue(val):
+#     feature_dict = {"No": 1, "Yes": 2}
+#     for key, value in feature_dict.items():
+#         if val == key:
+#             return value
+
+# def get_value(val, my_dict):
+#     for key, value in my_dict.items():
+#         if val == key:
+#             return value
+
+# app_mode = st.sidebar.selectbox('Select Page', ['Home', 'Prediction'])  # two pages
+
+# if app_mode=='Home':    
+#     st.title('Laptop Price PREDICTION :') 
+#     st.image('Images\laptop.jpg',width=600)
+
+# elif app_mode == 'Prediction':
+#     st.image('Images\laptop.jpg',width=600)
+#     st.text("Sir/Maam,YOU need to fill all the neccesary informations in order to know the price!")
+
+#     st.sidebar.multiselect('choose a Brand', ['ASUS', 'Dell', 'HP', 'Apple'])
+#     st.sidebar.multiselect('choose RAM',['8GB', '16GB', '32GB','64GB'])
+#     st.sidebar.multiselect('choose Processor',['Intel', 'AMD'])
+#     st.sidebar.multiselect('choose Processor Core ',['Core i3', 'Core i5', 'Core i7'])
+#     st.button("Predict")
