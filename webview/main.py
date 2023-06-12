d90bca6758c7957b2716b039ad6a7d6a65a49e31
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-06-12 13:49:28 +0600

    webview updated

--- webview/main.py
+++ webview/main.py
@@ -5,18 +5,18 @@ import numpy as np
 # importing the model and the pickle dataset 
 df = pickle.load(open("dataset.pkl", "rb"))
 
-left_column, middle_column, right_column = st.columns(3)
+left_column, middle_column, right_column, corner_column = st.columns(4)
 with left_column:
-    # brand input
+    # laptop brand
     laptop_brand = st.selectbox("Brand", df["Brand"].unique())
 
-# with middle_column:
-#     # laptop type
-#     type = st.selectbox("Type", df["TypeName"].unique())
+with middle_column:
+    # processor brand
+    pro_brand = st.selectbox("Processor Brand", df["Processor Brand"].unique())
 
-# with right_column:
-#     # Ram size
-#     ram = st.selectbox("Ram (in GB)", df["Ram"].unique())
+with right_column:
+    # Ram size
+    ram = st.selectbox("Processor Model", df["Processor Model"].unique())
 
 # st.sidebar.title("Welcome to LapTopPricePredix")
 # @st.cache(suppress_st_warning=True)
@@ -41,7 +41,7 @@ with left_column:
 #     st.image('Images\laptop.jpg',width=600)
 #     st.text("Sir/Maam,YOU need to fill all the neccesary informations in order to know the price!")
 
-#     st.sidebar.multiselect('choose a Brand', ['ASUS', 'Dell', 'HP', 'Apple'])
+    # st.sidebar.multiselect('choose a Brand', ['ASUS', 'Dell', 'HP', 'Apple'])
 #     st.sidebar.multiselect('choose RAM',['8GB', '16GB', '32GB','64GB'])
 #     st.sidebar.multiselect('choose Processor',['Intel', 'AMD'])
 #     st.sidebar.multiselect('choose Processor Core ',['Core i3', 'Core i5', 'Core i7'])
